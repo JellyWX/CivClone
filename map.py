51dd389f47d06562ebf275f1690882bd47e21b60
JellyWX <judewrs@gmail.com>
2018-01-17 21:09:18 +0000

    tried at some rotating earth stuff. still WIP

--- map.py
+++ map.py
@@ -51,25 +51,33 @@ class Map(Title):
 
     top = self.delta[1]
     bottom = top + self.gui.height
+    left = self.delta[0]
+    right = left + self.gui.width
 
-    high_row = top // (th/2)
+    high_row = (top // (th/2)) - 1
     low_row = (bottom // (th/2)) + 1
 
-    high_row, low_row = map(int, (high_row, low_row))
+    left_col = left // (tw/2) - 1
+    right_col = right // (tw/2) + 1
+
+    high_row, low_row, left_col, right_col = map(int, (high_row, low_row, left_col, right_col))
 
     if high_row < 0:
       high_row = 0
     if low_row >= self.height:
       low_row = self.height - 1
 
-    print(high_row, low_row)
+    if right_col >= self.width:
+      right_col %= self.width
 
-    current_tile = 0
+    print(left_col, right_col)
+
+    current_tile = left_col
     current_row = high_row
     invert = high_row % 2
 
     for row in self.map[high_row:low_row]:
-      for tile in row:
+      for tile in row[left_col:right_col]:
         if (tw * current_tile) - self.delta[0] < self.gui.width and 0 < tw * (current_tile + 2) - self.delta[0]:
           tile.render(current_tile * tw - self.delta[0], current_row * th/2 - self.delta[1], tw, th, outline, invert)
 
